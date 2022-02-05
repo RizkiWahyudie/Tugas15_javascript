9bfc6f9eef8153bd08aeca7d4b18cc8600502320
rizki wahyudie git <rizkiwahyudie@gmail.com>
2022-02-05 16:42:40 +0700

    add file

---
+++ tugas15.js
@@ -0,0 +1,15 @@
+var ab = 8;
+var bc = 6;
+console.log("Nilai AB: 8");
+console.log("Nilai BC: 6");
+
+// 8 kuadrat = 64
+console.log("8 kuadrat:",Math.pow(ab,2));
+// 6 kuadrat = 36
+console.log("6 kuadrat:",Math.pow(bc,2));
+// 6 kuadrat + 8 kuadrat = 100
+console.log("64 + 36:",(Math.pow(ab,2))+(Math.pow(bc,2)));
+// Akar dari hasil pertambahan diatas = 10
+console.log("√100:",Math.sqrt((Math.pow(ab,2))+(Math.pow(bc,2))));
+
+console.log("Panjang sisi AC dari segitiga siku-siku tersebut adalah: 10cm");
